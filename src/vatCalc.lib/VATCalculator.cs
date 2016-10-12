062533fe9633a913cf78defe13dc537c17ca7713
Rasmus Lauridsen <rasmus@lauridsen.me>
2016-10-12 04:59:11 +0200

    initial commit by T Rump
    This is a perfect vat calculator
    I tell you it is perfect, because I know it it perfect. Believe me it
    is perfect.

---
+++ src/vatCalc.lib/VATCalculator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace vatCalc.lib
+{
+    public static class VATCalculator
+    {
+        public static double AddVAT(double originalPrice, double vatPercentage)
+        {    
+            var vat = CalculateVAT(originalPrice, vatPercentage);
+            return originalPrice + vat;
+        }
+
+        public static double SubtractVAT(double originalPrice, double vatPercentage)
+        {    
+            var vat = CalculateVAT(originalPrice, vatPercentage);
+            return originalPrice - vat;
+        }
+
+        private static double CalculateVAT(double price, double vatPercentage)
+        {
+            return (price / 100) * vatPercentage;
+        }
+    }
+}
---
+++ src/vatCalc/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using vatCalc.lib;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var operation = args[0];
+            var price = double.Parse(args[1]);
+            var vatPercentage = double.Parse(args[2]);
+
+            if(operation == "add")
+            {
+                var result = VATCalculator.AddVAT(price, vatPercentage);
+                Console.WriteLine(result);
+            }else if( operation == "subtract")
+            {
+                var result = VATCalculator.SubtractVAT(price, vatPercentage);
+                Console.WriteLine(result);
+            }else
+            {
+                Console.WriteLine($"{operation} not a valid operation (add, subtract)");
+            }
+        }
+    }
+}
