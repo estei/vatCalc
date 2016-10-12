4ecc9643dabf5114306a96f3be4cf470e3d1b587
Rasmus Lauridsen <rasmus@lauridsen.me>
2016-10-12 13:46:44 +0200

    fixed code

--- src/vatCalc.lib/VATCalculator.cs
+++ src/vatCalc.lib/VATCalculator.cs
@@ -12,8 +12,7 @@ namespace vatCalc.lib
 
         public static decimal SubtractVAT(decimal originalPrice, decimal vatPercentage)
         {    
-           var vat = CalculateVAT(originalPrice, vatPercentage);
-           return originalPrice - vat;
+            return (originalPrice / ( 100 + vatPercentage )) * 100;
         }
 
         private static decimal CalculateVAT(decimal price, decimal vatPercentage)
